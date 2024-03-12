91ba5227d7cbda43d89276165144ab167e88929f
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-03-12 14:59:50 +0530

    Fix Some Error in Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"]
+  apiRoutes: [],
+  publicRoutes: ["/", "/api/tasks"],
 });
 
 export const config = {
